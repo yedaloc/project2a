7fc9aef3aa6298128ea601e69974318e8af941c5
Azucena Monges <azumonges@fpuna.com.py>
2022-10-26 23:41:55 -0300

    translate_foods

--- src/components/NavCards.js
+++ src/components/NavCards.js
@@ -34,6 +34,25 @@ export function NavCards() {
                     <p className="center">Plan</p>
                 </NavLink>
                 </div>
+
+            {/* Navigation card for view plan */}
+            <div className="flex-item-card cards-color">
+                <NavLink className = "navlink" to="/viewplan">
+                    <div>
+                        <span className="material-icons md-48 center margin-card-title">calendar_month</span>
+                    </div>
+                    <p className="center">Agendar Cita</p>
+                </NavLink>
+                </div>
+
+                <div className="flex-item-card cards-color">
+                <NavLink className = "navlink" to="/viewplan">
+                    <div>
+                        <span className="material-icons md-48 center margin-card-title">calendar_month</span>
+                    </div>
+                    <p className="center">Mis Citas</p>
+                </NavLink>
+                </div>
             </div>
     )
 }
